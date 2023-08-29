a680117027e909fc75cc4cce5bacd8247b14b629
MhdTarhini <mohamedtarhini95@gmail.com>
2023-08-29 21:51:17 +0300

    Resize Image using Resize-img and send success message

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+const resizeImg = require("resize-img");
 
 const isMac = process.platform === "darwin";
 
+process.env.NODE_ENV = "production";
+
 const isDev = process.env.NODE_ENV !== "production";
 
+let mainWindow;
 //Create main window
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "Image Resizer",
     width: isDev ? 1000 : 500,
     height: 800,
@@ -42,6 +46,8 @@ function createAboutWindow() {
 app.whenReady().then(() => {
   createMainWindow();
 
+  // remove mainWindow from memory on close
+  mainWindow.on("closed", () => (mainWindow = null));
   //implement menu
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
@@ -101,9 +107,37 @@ const menu = [
 
 // responde to ipcRenderer resize
 ipcMain.on("image:resize", (e, options) => {
-  options.dest = path.join(os.homedir);
-  console.log(options);
+  options.dest = path.join(os.homedir(), "imageresizer");
+  resizeImage(options);
 });
+
+async function resizeImage({ img_path, width, height, dest }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(img_path), {
+      width: +width,
+      height: +height,
+    });
+
+    //create file name
+    const filename = path.basename(img_path);
+
+    //create dest folder if not exists
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+
+    //write file to dest
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    // send Success message to render
+    mainWindow.webContents.send("image:done");
+
+    //open dest folder
+    shell.openPath(dest);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) {
--- renderer/js/script.js
+++ renderer/js/script.js
@@ -48,6 +48,11 @@ function sendImage(e) {
   });
 }
 
+// Catch the image:done event
+ipcRenderer.on("imge:done", () => {
+  alertSuccess(`Image resized to ${widthInput.value} x ${heightInput.value}`);
+});
+
 function isFileImage(file) {
   const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
   return file && acceptedImageTypes.includes(file["type"]);
